217abfe6253dc4bd0642e7e2760e6f161ded29fd
Pranjal Paliwal <paliwal.pranjal83@gmail.com>
2025-10-08 20:06:01 +0000

    chore: Disable demo workflow bootstrap on startup
    - Removed WorkflowsBootstrapService from providers
    - Workflows should be created manually via API/UI
    - All tests still passing (20/20)
    - Backend and worker running clean

--- backend/src/workflows/workflows.module.ts
+++ backend/src/workflows/workflows.module.ts
@@ -5,12 +5,12 @@ import { TemporalModule } from '../temporal/temporal.module';
 import { WorkflowRepository } from './repository/workflow.repository';
 import { WorkflowsController } from './workflows.controller';
 import { WorkflowsService } from './workflows.service';
-import { WorkflowsBootstrapService } from './workflows.bootstrap';
+// import { WorkflowsBootstrapService } from './workflows.bootstrap';
 
 @Module({
   imports: [DatabaseModule, TemporalModule],
   controllers: [WorkflowsController],
-  providers: [WorkflowsService, WorkflowRepository, WorkflowsBootstrapService],
+  providers: [WorkflowsService, WorkflowRepository], // Removed WorkflowsBootstrapService
   exports: [WorkflowsService],
 })
 export class WorkflowsModule {}
