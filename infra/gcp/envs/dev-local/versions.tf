6ef25fcb5bfddcbe8b4541bfd44771ab7c2f5bff
betterclever <paliwal.pranjal83@gmail.com>
2026-02-09 14:28:10 +0400

    feat(infra): add dev-local env without remote state
    Signed-off-by: betterclever <paliwal.pranjal83@gmail.com>
    
    Signed-off-by: betterclever <paliwal.pranjal83@gmail.com>

---
+++ infra/gcp/envs/dev-local/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.20.0"
+    }
+  }
+}
+
+provider "google" {
+  project      = var.project_id
+  region       = var.region
+  zone         = var.zone
+  access_token = var.access_token
+}
+
